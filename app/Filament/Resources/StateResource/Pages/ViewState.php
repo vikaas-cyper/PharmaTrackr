e6ae694aed77c1f40cb8408a78f7d6ccfe54220f
vikaas-cyper <avikaas17@gmail.com>
2023-09-13 11:18:06 +0530

    State resource created

---
+++ app/Filament/Resources/StateResource/Pages/ViewState.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\StateResource\Pages;
+
+use App\Filament\Resources\StateResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewState extends ViewRecord
+{
+    protected static string $resource = StateResource::class;
+}

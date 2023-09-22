267eddca2b1fb14ba64a144a5bbb9c01b4706005
vikaas-cyper <avikaas17@gmail.com>
2023-09-22 10:17:51 +0530

    State view

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -38,6 +38,8 @@ class StateResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
